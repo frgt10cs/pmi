c6bad9d46b8542e7078ef77b74f84bfccf2ae6da
Danil <danil.lazarev.90@mail.ru>
2020-02-23 18:30:24 +0300

    Доработан класс Discipline

--- Pmi/Model/Discipline.cs
+++ Pmi/Model/Discipline.cs
@@ -9,36 +9,33 @@ namespace Pmi.Model
     class Discipline
     {
         public string Name { get; }
-        public int Flow { get; set; }
-        public int Groups { get; set; }
+        public List<string> Groups { get; set; }
+        public string CodeOP { get; set; } 
         public double Lectures { get; set; }
         public double PracticalWork { get; set; }
         public double LaboratoryWork { get; set; }
-        public double Consultations { get; set; }
-        public double WorkWithAspirants { get; set; }
+        public double ConsultationsByTheory { get; set; }
+        public double ConsultationsByDiplom { get; set; }
+        public double Aspirants { get; set; }
         public double Coursework { get; set; }
         public double Diploms { get; set; }
         public double Practice { get; set; }
+        public double GEK { get; set; }
         public double Tests { get; set; }
         public double Exam { get; set; }
-        public double Aspirants { get; set; }
-        public double Masters { get; set; }
-        public double Gak { get; set; }
+        public double AnotherWork { get; set; }
 
         public Discipline(string name)
         {
             Name = name;
+            Groups = new List<string>();
         }
-        public string FlowAndCountOfGroups()
-        {
-            return $"{Flow}({Groups})";
-        }
-
+ 
         public double TotalForThisDiscipline()
         {
-            return Lectures + PracticalWork + LaboratoryWork + Consultations +
-                WorkWithAspirants + Coursework + Diploms + Practice + Tests +
-                Exam + Aspirants + Masters + Gak;
+            return Lectures + PracticalWork + LaboratoryWork + ConsultationsByTheory +
+                ConsultationsByDiplom + Coursework + Diploms + Practice + Tests +
+                Exam + Aspirants + GEK+ AnotherWork;
         }
     }
 }
