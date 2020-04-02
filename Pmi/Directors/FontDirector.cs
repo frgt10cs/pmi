097a42e97a8e866ddfb7e2d2f4fe2d07b5dba9e2
Danil <danil.lazarev.90@mail.ru>
2020-04-02 17:28:28 +0300

    Добавлены стили

--- Pmi/Directors/FontDirector.cs
+++ Pmi/Directors/FontDirector.cs
@@ -37,7 +37,82 @@ namespace Pmi.Directors
         public void BuildEmployeeInfo()
         {
             fontBuilder.SetFontSize(14);
-            fontBuilder.AddUnderline(DocumentFormat.OpenXml.Spreadsheet.UnderlineValues.Single);            
+            fontBuilder.AddUnderline(DocumentFormat.OpenXml.Spreadsheet.UnderlineValues.Single);
+        }
+
+        public void BuildColumnName()
+        {
+            fontBuilder.SetFontSize(11);
+        }
+
+        public void BuildTotal()
+        {
+            fontBuilder.SetFontSize(11);
+            fontBuilder.AddBold();
+        }
+
+        public void BuildColumnNumber()
+        {
+            fontBuilder.SetFontSize(10);
+        }
+
+        public void BuildSemesterName()
+        {
+            fontBuilder.SetFontSize(12);
+        }
+
+        public void BuildDisciplineCode()
+        {
+            fontBuilder.SetFontSize(10);
+        }
+
+        public void BuildGroupPlan()
+        {
+            fontBuilder.SetFontSize(10);
+        }
+
+        public void BuildDisciplineName()
+        {
+            fontBuilder.SetFontSize(10);
+        }
+
+        public void BuildColumnTotal()
+        {
+            fontBuilder.SetFontSize(11);
+        }
+
+        public void BuildSemesterTotalName()
+        {
+            fontBuilder.SetFontSize(12);
+        }
+
+        public void BuildApprove()
+        {
+            fontBuilder.SetFontSize(14);
+        }
+        public void BuildPosition()
+        {
+            fontBuilder.SetFontSize(14);
+        }
+        public void BuildDepatment()
+        {
+            fontBuilder.SetFontSize(14);
+            fontBuilder.AddUnderline(DocumentFormat.OpenXml.Spreadsheet.UnderlineValues.Single);
+        }
+        public void BuildManagerInfo()
+        {
+            fontBuilder.SetFontSize(12);
+            fontBuilder.AddItalic();
+        }
+        public void BuildManagerInfoMeta()
+        {
+            fontBuilder.SetFontSize(8);
+            fontBuilder.AddItalic();
+        }
+
+        public void BuildTeacherSignature()
+        {
+            fontBuilder.SetFontSize(8);
         }
     }
 }
